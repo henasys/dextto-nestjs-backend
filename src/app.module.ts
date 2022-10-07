4c57574ab7959644a1fd8cc9f82a07203cc07cf5
Alex Hojong Lee <alexhojonglee@gmail.com>
2022-10-07 23:42:46 +0900

    ch6.4 environment variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
-import { EmailModule } from './email/email.module';
+import { ConfigModule } from '@nestjs/config';
+import emailConfig from './config/email.config';
+import { validationSchema } from './config/validation.schema';
 
 @Module({
-  imports: [UsersModule, EmailModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      load: [emailConfig],
+      isGlobal: true,
+      validationSchema,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
